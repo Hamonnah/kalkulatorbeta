4030daa7f9284ef3064d41ed95c3bf58dcbb6954
Monika Jozefina <kozefina@MacBook-Air-Monika.local>
2020-06-04 21:41:37 +0200

    first commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,12 +2,12 @@ public class Calculator {
 
     public int addNumbers(int x, int y) {
         int totalAddNumbers;
-        return totalAddNumbers = x + y;
+        return x + y;
     }
 
     public int substractNumbers(int x, int y) {
         int totalSubstractNumbers;
-        return totalSubstractNumbers = x - y;
+        return x - y;
     }
 
 }
