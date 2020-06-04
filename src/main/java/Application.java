b798f24d77a31b0329a02aed23a838f4be75330d
Monika Jozefina <kozefina@MacBook-Air-Monika.local>
2020-06-04 10:18:03 +0200

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addNumbers(10, 15));
+        System.out.println(calculator.substractNumbers(78, 98));
+    }
+
+}
---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public int addNumbers(int x, int y) {
+        int totalAddNumbers;
+        return totalAddNumbers = x + y;
+    }
+
+    public int substractNumbers(int x, int y) {
+        int totalSubstractNumbers;
+        return totalSubstractNumbers = x - y;
+    }
+
+}
